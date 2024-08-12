56fe2676f244c92add65606a680b88275bffe35f
WackyMole <wacky@wackymole.com>
2024-08-12 15:58:47 -0500

    some adjustments

--- LevelSystem/LevelSystem_noncombat.cs
+++ LevelSystem/LevelSystem_noncombat.cs
@@ -4,35 +4,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static UnityEngine.UI.GridLayoutGroup;
 
 namespace EpicMMOSystem
 {
     internal static class LevelSystem_noncombat
     {
-        
+
         [HarmonyPatch(typeof(Destructible), nameof(Destructible.RPC_Damage))]
         private static class Destructible_dmg_patch
         {
             private static void Postfix(Destructible __instance, HitData hit)
             {
                 if (EpicMMOSystem.disableNonCombatObjects.Value) return;
-                if (!__instance.m_nview.IsOwner()) return;     
-                if(EpicMMOSystem.debugNonCombatObjects.Value)
+                if (!__instance.m_nview.IsOwner()) return;
+                if (EpicMMOSystem.debugNonCombatObjects.Value)
                     EpicMMOSystem.MLLogger.LogWarning("Destructible name" + __instance.gameObject.name);
                 if (!__instance.m_destroyed) return;
-                if (!DataMonsters.contains(__instance.gameObject.name)) return;
 
-                    Character attacker = hit.GetAttacker();
-                    if (attacker == null) return;
-                    if (attacker is not Player player) return;
-                    if (!hit.CheckToolTier(__instance.m_minToolTier)) return;
-                    if (hit.GetTotalDamage() < 1) return;
-                    int expMonster = DataMonsters.getExp(__instance.gameObject.name);
-                    LevelSystem.Instance.AddExp(expMonster);
-                
+                if (!DataMonsters.contains(__instance.gameObject.name)) return;
+                Character attacker = hit.GetAttacker();
+                if (attacker == null) return;
+                if (attacker is not Player player) return;
+                if (!hit.CheckToolTier(__instance.m_minToolTier)) return;
+                if (hit.GetTotalDamage() < 1) return;
+                int expMonster = DataMonsters.getExp(__instance.gameObject.name);
+                LevelSystem.Instance.AddExp(expMonster);
+
 
             }
         }
+
+        [HarmonyPatch(typeof(Pickable), nameof(Pickable.RPC_Pick))]
+        private static class PickablePickMMOWacky
+        {
+            private static void Postfix(Pickable __instance)
+            {
+
+                if (EpicMMOSystem.disableNonCombatObjects.Value) return;
+                if (EpicMMOSystem.debugNonCombatObjects.Value)
+                    EpicMMOSystem.MLLogger.LogWarning("pickable name" + __instance.name);
+
+                if (!DataMonsters.contains(__instance.name)) return;
+                int expMonster = DataMonsters.getExp(__instance.name);
+                LevelSystem.Instance.AddExp(expMonster);
+            }
+        }
+
+
 
         [HarmonyPatch(typeof(MineRock), nameof(MineRock.RPC_Hit))]
         private static class MineRock_dmg_patch
@@ -87,7 +106,7 @@ namespace EpicMMOSystem
                         case PlayerStatType.Tree:
                             if (SeeifDied[PlayerStatType.Tree] == 1) SeeifDied[PlayerStatType.Tree] = 2;
                             break;
-                            
+
                         case PlayerStatType.LogChops:
                             if (SeeifDied[PlayerStatType.LogChops] == 1) SeeifDied[PlayerStatType.LogChops] = 2;
                             break;
@@ -102,21 +121,21 @@ namespace EpicMMOSystem
         [HarmonyPatch(typeof(TreeBase), nameof(TreeBase.RPC_Damage))]
         private static class TreeBase_dmg_patch
         {
-           private static void Prefix()
+            private static void Prefix()
             {
-                    SeeifDied[PlayerStatType.Tree] = 1;
+                SeeifDied[PlayerStatType.Tree] = 1;
             }
             private static void Postfix(TreeBase __instance, HitData hit)
             {
                 if (EpicMMOSystem.disableNonCombatObjects.Value) return;
                 if (EpicMMOSystem.debugNonCombatObjects.Value)
-                    EpicMMOSystem.MLLogger.LogWarning("Treebase name" + __instance.gameObject.name );
-                    if (SeeifDied[PlayerStatType.Tree] == 1)
-                    {
-                        SeeifDied[PlayerStatType.Tree] = 0;
-                        return;
-                    }
+                    EpicMMOSystem.MLLogger.LogWarning("Treebase name" + __instance.gameObject.name);
+                if (SeeifDied[PlayerStatType.Tree] == 1)
+                {
                     SeeifDied[PlayerStatType.Tree] = 0;
+                    return;
+                }
+                SeeifDied[PlayerStatType.Tree] = 0;
 
                 if (!DataMonsters.contains(__instance.gameObject.name)) return;
                 Character attacker = hit.GetAttacker();
@@ -141,7 +160,7 @@ namespace EpicMMOSystem
             {
                 if (EpicMMOSystem.disableNonCombatObjects.Value) return;
                 if (!__instance.m_nview.IsOwner()) return;
-                if (EpicMMOSystem.debugNonCombatObjects.Value)        
+                if (EpicMMOSystem.debugNonCombatObjects.Value)
                     EpicMMOSystem.MLLogger.LogWarning("TreeLog name" + __instance.gameObject.name);
                 /*if (SeeifDied[PlayerStatType.Logs] == 1 || SeeifDied[PlayerStatType.LogChops] == 1)
                 {
@@ -159,7 +178,7 @@ namespace EpicMMOSystem
                 int expMonster = DataMonsters.getExp(__instance.gameObject.name);
                 LevelSystem.Instance.AddExp(expMonster);
             }
-            
+
         }
 
         [HarmonyPatch(typeof(Tameable), nameof(Tameable.Tame))]
@@ -172,39 +191,56 @@ namespace EpicMMOSystem
                 Player closestPlayer = Player.GetClosestPlayer(__instance.transform.position, 30f);
                 if (!closestPlayer) return;
                 int expMonster = DataMonsters.getExp(__instance.gameObject.name);
-                LevelSystem.Instance.AddExp(expMonster* EpicMMOSystem.MultiplierForXPTaming.Value);
+                LevelSystem.Instance.AddExp(expMonster * EpicMMOSystem.MultiplierForXPTaming.Value);
             }
         }
 
-        [HarmonyPatch(typeof(Player), nameof(Player.PlacePiece))]      
+        private static string lastpiece = "";
+        [HarmonyPatch(typeof(Player), nameof(Player.PlacePiece))]
         private static class Player_placepiece_patch_epicmmoA
         {
             private static void Postfix(Player __instance, Piece piece, ref bool __result)
             {
-                if (EpicMMOSystem.disableNonCombatObjects.Value) return;            
-                if (EpicMMOSystem.debugNonCombatObjects.Value)
-                    EpicMMOSystem.MLLogger.LogWarning("piece name" + piece.name);
-                 if( !__result) return;
-                if (!DataMonsters.contains(piece.name+ "(Clone)")) return;  
-                int expMonster = DataMonsters.getExp(piece.name + "(Clone)");
-                LevelSystem.Instance.AddExp(expMonster);
+                if (EpicMMOSystem.disableNonCombatObjects.Value) return;
+                if (piece.m_cultivatedGroundOnly)
+                {
+                    if (EpicMMOSystem.debugNonCombatObjects.Value)
+                        EpicMMOSystem.MLLogger.LogWarning("plant name" + piece.name);
+
+                    if (!DataMonsters.contains(piece.name + "(Clone)")) return;
+                    int expMonster = DataMonsters.getExp(piece.name + "(Clone)");
+                    LevelSystem.Instance.AddExp(expMonster);
+                }
+                else
+                {
+                    if (lastpiece == piece.name) return;
+                    lastpiece = piece.name;
+                    if (EpicMMOSystem.debugNonCombatObjects.Value)
+                        EpicMMOSystem.MLLogger.LogWarning("piece name" + piece.name);
+                    if (!__result) return;
+                    if (!DataMonsters.contains(piece.name + "(Clone)")) return;
+                    int expMonster = DataMonsters.getExp(piece.name + "(Clone)");
+                    LevelSystem.Instance.AddExp(expMonster);
+                }
             }
         }
 
         [HarmonyPatch(typeof(Fish), nameof(Fish.OnHooked))]
-        private static class Fish_Caught_patch
+        private static class Fish_Hook_patch
         {
             private static void Postfix(Fish __instance)
             {
                 if (EpicMMOSystem.disableNonCombatObjects.Value) return;
-               // if (!__instance) return;
+                // if (!__instance) return;
                 if (__instance.m_fishingFloat == null) return;
                 if (EpicMMOSystem.debugNonCombatObjects.Value)
                     EpicMMOSystem.MLLogger.LogWarning("fish name" + __instance.gameObject.name);
+
                 if (!DataMonsters.contains(__instance.gameObject.name)) return;
                 Character owner = __instance.m_fishingFloat.GetOwner();
                 if (owner == null) return;
                 if (owner is not Player player) return;
+
                 int expMonster = DataMonsters.getExp(__instance.gameObject.name);
                 int maxExp = DataMonsters.getMaxExp(__instance.gameObject.name);
                 float lvlExp = EpicMMOSystem.expForLvlMonster.Value;
@@ -214,6 +250,43 @@ namespace EpicMMOSystem
                 var playerExp = exp;
                 LevelSystem.Instance.AddExp(playerExp);
             }
-        }       
+        }
+        private static string lastfishname = "";
+        [HarmonyPatch(typeof(Fish), nameof(Fish.Interact))]
+        private static class Fish_Caught_patch_interact
+        {
+            private static void Postfix(Fish __instance, Humanoid character, bool repeat, bool alt)
+            {
+                if (EpicMMOSystem.disableNonCombatObjects.Value) return;
+                if (!__instance) return;
+
+                if (repeat)
+                {
+                    return ;
+                }
+
+                if (character == null) return;
+                if (character is not Player player) return;
+
+
+                if (EpicMMOSystem.debugNonCombatObjects.Value)
+                    EpicMMOSystem.MLLogger.LogWarning("fish Interact name" + __instance.name);
+                if (!DataMonsters.contains(__instance.name)) return;
+
+                if (__instance.gameObject.name == lastfishname) return; // cant pickup fish over and over
+                lastfishname = __instance.gameObject.name;
+
+                int expMonster = DataMonsters.getExp(__instance.gameObject.name);
+                int maxExp = DataMonsters.getMaxExp(__instance.gameObject.name);
+                float lvlExp = EpicMMOSystem.expForLvlMonster.Value;
+                int monsterLevel = DataMonsters.getLevel(__instance.gameObject.name);
+                var resultExp = expMonster + (maxExp * lvlExp * (monsterLevel - 1));
+                var exp = Convert.ToInt32(resultExp);
+                var playerExp = exp;
+                LevelSystem.Instance.AddExp(playerExp);
+
+            }
+
+        }
     }
 }
--- Plugin.cs
+++ Plugin.cs
@@ -37,7 +37,7 @@ namespace EpicMMOSystem;
 public partial class EpicMMOSystem : BaseUnityPlugin
 {
     internal const string ModName = "EpicMMOSystem";
-    internal const string VERSION = "1.9.20";
+    internal const string VERSION = "1.9.21";
     internal const string Author = "WackyMole";
    // internal const string configV = "_1_7";
     private const string ModGUID = Author + "." + ModName; //+ configV; changes GUID
